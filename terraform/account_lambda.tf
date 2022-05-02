b841ca48be380d88453291b8608b4df06e567395
Playground <playground@Libor-A220314-2.local>
2022-05-02 22:08:18 +1200

    Attempt to fix pipeline

--- terraform/account_lambda.tf
+++ terraform/account_lambda.tf
@@ -1,7 +1,7 @@
 data "archive_file" "account_lambda" {
   type = "zip"
 
-  source_dir  = "${path.module}/../lambdas/account/build"
+  source_dir  = "${path.module}/../lambdas/account/dist"
   output_path = "${path.module}/account_lambda.zip"
 }
 
