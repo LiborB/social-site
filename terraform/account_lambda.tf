7bcb39421b78dc1464660302b1fca317c4736aee
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-04 18:06:37 +1200

    test

--- terraform/account_lambda.tf
+++ terraform/account_lambda.tf
@@ -17,7 +17,7 @@ resource "random_string" "r" {
 resource "aws_lambda_function" "account_lambda" {
   function_name = "account-lambda"
 
-  filename = "account_lambda.zip"
+  filename = data.archive_file.account_lambda.output_path
 
   runtime = "nodejs14.x"
   handler = "index.handler"
