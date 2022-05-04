af6277018c51d3dff432b383dfbe95a880ce09ca
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-04 20:44:11 +1200

    test

--- lambdas/account/index.ts
+++ lambdas/account/index.ts
@@ -8,14 +8,6 @@ router.get("/account", (req, res) => {
     return res.status(200).json("Hello World")
 })
 
-router.get("/", (req, res) => {
-    return res.status(200).json("wtf is this")
-})
-
-router.get("/v1/account", (req, res) => {
-    return res.status(200).json("v1 account")
-})
-
 app.use("/v1", router)
 
 export const handler = serverlessExpress({
