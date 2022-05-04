42c9253c41efda47bc45e81b4f51d0111f5b6474
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-04 21:19:04 +1200

    test

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -13,7 +13,7 @@ resource "aws_apigatewayv2_deployment" "lambda" {
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
   protocol_type = "HTTP"
-  version = "1"
+  version       = "1"
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
