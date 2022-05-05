b819d46fc5cc3c6e003fb6275a5c50b960c038b8
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-05 21:37:08 +1200

    format

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,6 +1,38 @@
-module "cdn" {
-  source  = "cloudposse/cloudfront-s3-cdn/aws"
-  version = "0.82.4"
+locals {
+  s3_origin_id = "socialsite"
+}
 
-  origin_bucket = aws_s3_bucket.website_bucket.id
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["HEAD", "GET"]
+    target_origin_id = local.s3_origin_id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
