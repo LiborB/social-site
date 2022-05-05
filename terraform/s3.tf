1481f507ff90c10ce41dd1a7c46036f072080296
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-05 22:37:26 +1200

    format

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,14 +6,14 @@ resource "aws_s3_bucket" "website_bucket" {
 resource "aws_s3_bucket_object" "website_bucket_dist" {
   for_each = fileset("${path.module}/../client-app/dist/client-app", "*")
 
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = aws_s3_bucket.website_bucket.bucket
   key    = each.value
   source = "${path.module}/../client-app/dist/client-app/${each.value}"
   etag   = filemd5("${path.module}/../client-app/dist/client-app/${each.value}")
 }
 
 resource "aws_s3_bucket_website_configuration" "website_bucket" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = aws_s3_bucket.website_bucket.bucket
 
   index_document {
     suffix = "index.html"
