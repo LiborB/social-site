6496b79795d1e990559e2802ef06270991b10f35
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-05 20:12:03 +1200

    Setup frontend

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,10 +4,10 @@ resource "aws_s3_bucket" "website_bucket" {
 }
 
 resource "aws_s3_bucket_object" "website_bucket_dist" {
-  for_each = fileset("${path.module}/../frontend/client-app/dist/client-app", "*")
+  for_each = fileset("${path.module}/../client-app/dist/client-app", "*")
 
   bucket = "website-bucket"
   key    = each.value
-  source = "${path.module}/../frontend/client-app/dist/client-app/${each.value}"
-  etag   = filemd5("${path.module}/../frontend/client-app/dist/client-app/${each.value}")
+  source = "${path.module}/../client-app/dist/client-app/${each.value}"
+  etag   = filemd5("${path.module}/../client-app/dist/client-app/${each.value}")
 }
