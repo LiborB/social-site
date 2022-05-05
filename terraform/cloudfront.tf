35ad49537fd569e66a70f7fd4e18d4b38510c5d4
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-05 21:19:12 +1200

    format

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,5 +1,6 @@
 module "cdn" {
-  source = "cloudposse/cloudfront-s3-cdn/aws"
+  source  = "cloudposse/cloudfront-s3-cdn/aws"
+  version = "0.82.4"
 
   origin_bucket = aws_s3_bucket.website_bucket.id
 }
