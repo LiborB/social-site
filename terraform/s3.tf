cfd1374c860792501328822a28841e7f1aa68868
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-05 22:59:00 +1200

    format

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "social-site-website-bucket"
+  bucket = "social-site-website-bucket-${random_pet.bucket_name}"
   acl    = "public-read"
 }
 
