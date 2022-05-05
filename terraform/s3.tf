6e3003a90b21c909f56b01e259cb7e868fcae610
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-05 20:12:38 +1200

    Setup frontend

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,8 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "website-bucket"
-  acl    = "private"
 }
 
-resource "aws_s3_bucket_object" "website_bucket_dist" {
+resource "aws_s3_object" "website_bucket_dist" {
   for_each = fileset("${path.module}/../client-app/dist/client-app", "*")
 
   bucket = "website-bucket"
