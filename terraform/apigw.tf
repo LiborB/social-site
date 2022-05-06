c76e545a65b3c835d5c3aa4ba1f084aca711fbe3
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-06 17:48:59 +1200

    begin to add jwt handling

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -14,6 +14,12 @@ resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
   protocol_type = "HTTP"
   version       = "1"
+
+  cors_configuration {
+    allowed_origins = ["*"]
+    allow_methods = ["*"]
+    allow_headers = ["*"]
+  }
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
