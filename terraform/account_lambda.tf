6ace3907028765bf5a5c4010db970b400e9f2410
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-06 17:38:38 +1200

    begin to add jwt handling

--- terraform/account_lambda.tf
+++ terraform/account_lambda.tf
@@ -60,6 +60,23 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_role" "apigw_exec" {
+  name = "apigw_lambda_exec"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+    }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
