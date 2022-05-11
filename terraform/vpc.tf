a246b9b95818ff4e4ccbc0d63f9e6e82b40962b6
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-11 13:04:08 +1200

    fix

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "availability_zones" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
@@ -7,6 +9,7 @@ module "vpc" {
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
+  azs                  = data.aws_availability_zones.availability_zones.names
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
