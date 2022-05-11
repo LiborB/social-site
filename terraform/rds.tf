48037bdf58f8707a5dc923616f484a97460820ef
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-05-11 12:52:07 +1200

    fix

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -7,13 +7,13 @@ data "aws_ssm_parameter" "db_password" {
 }
 
 resource "aws_db_instance" "application_db" {
-  instance_class = "db.t4g.micro"
-  engine = "postgres"
-  username = data.aws_ssm_parameter.db_username.value
-  password = data.aws_ssm_parameter.db_password.value
-  allocated_storage = 20
-  engine_version = "14.1"
-  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
-  publicly_accessible    = true
-  skip_final_snapshot    = true
+  instance_class       = "db.t4g.micro"
+  engine               = "postgres"
+  username             = data.aws_ssm_parameter.db_username.value
+  password             = data.aws_ssm_parameter.db_password.value
+  allocated_storage    = 20
+  engine_version       = "14.1"
+  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+  publicly_accessible  = true
+  skip_final_snapshot  = true
 }
